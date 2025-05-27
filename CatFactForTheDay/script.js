8f7b5f46c386c31cd2bf67da27bda5b90fdfc3e0
Srisha <srisha15m@gmail.com>
2025-05-27 17:41:32 +0530

    Changes

--- CatFactForTheDay/script.js
+++ CatFactForTheDay/script.js
@@ -1,11 +1,18 @@
 let btn = document.querySelector("button");
 let p = document.querySelector("p");
+let h3 = document.querySelector("h3");
 
 let url = "https://catfact.ninja/fact";
+let counter = 0;
 
 btn.addEventListener("click", async ()=>{
     // let fact = await getFacts(); //returns Promise
     console.log("button was clicked")
+    if(counter == 10){
+        alert("Aren't you a Cat Lover!! Keep Meowingg");
+    }
+    counter++;
+    h3.innerText = `Facts read: ${counter}`
     let fact  = await getFacts();
     console.log(fact)
     p.innerText = fact
